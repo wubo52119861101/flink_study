9178a5f3aef5c663d194c85a26fe2083e8107181
wubo <wubo@wubo52119861101>
2020-01-08 15:48:27 +0800

    frist

--- src/main/java/org/flink/demo/FristFlinkDemo.java
+++ src/main/java/org/flink/demo/FristFlinkDemo.java
@@ -1,5 +1,20 @@
 package org.flink.demo;
 
+import org.apache.flink.streaming.api.scala.DataStream;
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment;
+
 public class FristFlinkDemo {
 
+    public static void main(String[] args) {
+
+        //1 初始化flink执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //2 读取指定路径的文本文件
+        DataStream<String> dataStream=env.readTextFile("/Users/wubo/Documents/GitHub/flink_study/test.txt");
+        //3 action算子 对DataStream中的数据打印
+        dataStream.print();
+        //4 启动flink应用
+        env.execute("test");
+
+    }
 }
