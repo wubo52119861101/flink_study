7bbc94520f6b7b26d9c290b4ff6dad9dd88c3250
wubo <wubo@wubo52119861101>
2020-01-08 20:07:22 +0800

    增加wordcount注释

--- src/main/java/org/flink/demo/WordCountDemo.java
+++ src/main/java/org/flink/demo/WordCountDemo.java
@@ -53,14 +53,15 @@ public class WordCountDemo {
             }
         });
 
-        //将元组按照key 进行分组
+        //将元组按照key 进行分组，将所有数据中包含该key的数据都发送到同一个分区上
         KeyedStream<WordWithCount, Tuple> grouped=pairsWords.keyBy("word");
 
         //调用窗口操作
-
+        //timeWindow时间窗口函数，以界定对多长时间内的数据做统计
         //需要给两个重要的参数:窗口长度和滑动间隔
         WindowedStream<WordWithCount,Tuple, TimeWindow> window=grouped.timeWindow(Time.seconds(3),Time.seconds(2));
-
+        //求和函数， 表示对二元组中第二个元素求和，因为经过前面的keyBy,所有的单词都被发到同一个分区上，因此在一个分区上
+        //将单词出现的次数进行累加，得到的就是单词出现的总次数
         SingleOutputStreamOperator<WordWithCount> counts=window.sum("count");
 
         //打印
